40494089bed0e402a91e23bc977af8386d822082
Shapoval <olekcandrsapoval41@gmail.com>
2022-12-10 23:34:29 +0200

    Ex_2

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import math
 try:
-    for i in range(1, 10):
-        for j in range(1, 10):
+    for i in range(1, 11):
+        for j in range(1, 11):
             print(i, '*', j, '=', i * j, end='\t\t')
         print()
 except Exception as ex:
